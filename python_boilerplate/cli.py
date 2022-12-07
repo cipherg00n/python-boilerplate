653ca12896d188712f7c1f60f436dc18be8de5c2
cipherg00n <cipherg00n@users.noreply.github.com>
2022-12-07 09:24:36 +0000

    ✅ Ready to clone and code.

--- python_boilerplate/cli.py
+++ python_boilerplate/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for python_boilerplate project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m python_boilerplate` and `$ python_boilerplate `.
 
     This is your program's entry point.
 
